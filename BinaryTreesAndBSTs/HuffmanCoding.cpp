0b1c4001fefe083d58867561925020b23995ff54
emaldonadogamedev <esteban.maldonado@digipen.edu>
2016-06-06 21:19:34 -0700

    slight change to BST problem

--- BinaryTreesAndBSTs/HuffmanCoding.cpp
+++ BinaryTreesAndBSTs/HuffmanCoding.cpp
@@ -1,5 +1,15 @@
 
-/*Huffman coding assigns variable length codewords to fixed length input characters based on their frequencies. More frequent characters are assigned shorter codewords and less frequent characters are assigned longer codewords. A Huffman tree is made for the input string and characters are decoded based on their position in the tree. We add a '0' to the codeword when we move left in the binary tree and a '1' when we move right in the binary tree. We assign codes to the leaf nodes which represent the input characters.
+/*
+
+Huffman coding assigns variable length codewords to fixed 
+length input characters based on their frequencies. More 
+frequent characters are assigned shorter codewords and less 
+frequent characters are assigned longer codewords. A Huffman 
+tree is made for the input string and characters are decoded 
+based on their position in the tree. We add a '0' to the codeword 
+when we move left in the binary tree and a '1' when we move right 
+in the binary tree. We assign codes to the leaf nodes which represent
+the input characters.
 
 For example :
 
